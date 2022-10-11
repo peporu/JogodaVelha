e6e7e114fbb8819c7d48621a4f59781d0b1bd4d6
PEDRO TRIMER RUGGIERO <pedro.truggiero@senacsp.edu.br>
2022-10-11 20:24:52 -0300

    Adicionar arquivos de projeto.

---
+++ JogodaVelha/frmPrincipal.Designer.cs
@@ -0,0 +1,224 @@
+﻿
+namespace JogodaVelha
+{
+    partial class frmPrincipal
+    {
+        /// <summary>
+        /// Variável de designer necessária.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpar os recursos que estão sendo usados.
+        /// </summary>
+        /// <param name="disposing">true se for necessário descartar os recursos gerenciados; caso contrário, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código gerado pelo Windows Form Designer
+
+        /// <summary>
+        /// Método necessário para suporte ao Designer - não modifique 
+        /// o conteúdo deste método com o editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.panel3 = new System.Windows.Forms.Panel();
+            this.panel4 = new System.Windows.Forms.Panel();
+            this.btn7 = new System.Windows.Forms.Button();
+            this.btn8 = new System.Windows.Forms.Button();
+            this.btn9 = new System.Windows.Forms.Button();
+            this.btn6 = new System.Windows.Forms.Button();
+            this.btn5 = new System.Windows.Forms.Button();
+            this.btn4 = new System.Windows.Forms.Button();
+            this.btn3 = new System.Windows.Forms.Button();
+            this.btn2 = new System.Windows.Forms.Button();
+            this.btn1 = new System.Windows.Forms.Button();
+            this.labelVitoria = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.panel1.Location = new System.Drawing.Point(25, 168);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(516, 19);
+            this.panel1.TabIndex = 0;
+            // 
+            // panel2
+            // 
+            this.panel2.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.panel2.Location = new System.Drawing.Point(25, 349);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(516, 19);
+            this.panel2.TabIndex = 1;
+            // 
+            // panel3
+            // 
+            this.panel3.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.panel3.Location = new System.Drawing.Point(181, 12);
+            this.panel3.Name = "panel3";
+            this.panel3.Size = new System.Drawing.Size(21, 512);
+            this.panel3.TabIndex = 2;
+            // 
+            // panel4
+            // 
+            this.panel4.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.panel4.Location = new System.Drawing.Point(364, 12);
+            this.panel4.Name = "panel4";
+            this.panel4.Size = new System.Drawing.Size(21, 512);
+            this.panel4.TabIndex = 3;
+            // 
+            // btn7
+            // 
+            this.btn7.Font = new System.Drawing.Font("Microsoft Sans Serif", 48F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn7.Location = new System.Drawing.Point(25, 12);
+            this.btn7.Name = "btn7";
+            this.btn7.Size = new System.Drawing.Size(150, 150);
+            this.btn7.TabIndex = 4;
+            this.btn7.UseVisualStyleBackColor = true;
+            this.btn7.Click += new System.EventHandler(this.btn7_Click);
+            // 
+            // btn8
+            // 
+            this.btn8.Font = new System.Drawing.Font("Microsoft Sans Serif", 48F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn8.Location = new System.Drawing.Point(208, 12);
+            this.btn8.Name = "btn8";
+            this.btn8.Size = new System.Drawing.Size(150, 150);
+            this.btn8.TabIndex = 5;
+            this.btn8.UseVisualStyleBackColor = true;
+            this.btn8.Click += new System.EventHandler(this.btn8_Click);
+            // 
+            // btn9
+            // 
+            this.btn9.Font = new System.Drawing.Font("Microsoft Sans Serif", 48F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn9.Location = new System.Drawing.Point(391, 12);
+            this.btn9.Name = "btn9";
+            this.btn9.Size = new System.Drawing.Size(150, 150);
+            this.btn9.TabIndex = 6;
+            this.btn9.UseVisualStyleBackColor = true;
+            this.btn9.Click += new System.EventHandler(this.btn9_Click);
+            // 
+            // btn6
+            // 
+            this.btn6.Font = new System.Drawing.Font("Microsoft Sans Serif", 48F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn6.Location = new System.Drawing.Point(391, 193);
+            this.btn6.Name = "btn6";
+            this.btn6.Size = new System.Drawing.Size(150, 150);
+            this.btn6.TabIndex = 9;
+            this.btn6.UseVisualStyleBackColor = true;
+            this.btn6.Click += new System.EventHandler(this.btn6_Click);
+            // 
+            // btn5
+            // 
+            this.btn5.Font = new System.Drawing.Font("Microsoft Sans Serif", 48F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn5.Location = new System.Drawing.Point(208, 193);
+            this.btn5.Name = "btn5";
+            this.btn5.Size = new System.Drawing.Size(150, 150);
+            this.btn5.TabIndex = 8;
+            this.btn5.UseVisualStyleBackColor = true;
+            this.btn5.Click += new System.EventHandler(this.btn5_Click);
+            // 
+            // btn4
+            // 
+            this.btn4.Font = new System.Drawing.Font("Microsoft Sans Serif", 48F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn4.Location = new System.Drawing.Point(25, 193);
+            this.btn4.Name = "btn4";
+            this.btn4.Size = new System.Drawing.Size(150, 150);
+            this.btn4.TabIndex = 7;
+            this.btn4.UseVisualStyleBackColor = true;
+            this.btn4.Click += new System.EventHandler(this.btn4_Click);
+            // 
+            // btn3
+            // 
+            this.btn3.Font = new System.Drawing.Font("Microsoft Sans Serif", 48F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn3.Location = new System.Drawing.Point(391, 374);
+            this.btn3.Name = "btn3";
+            this.btn3.Size = new System.Drawing.Size(150, 150);
+            this.btn3.TabIndex = 12;
+            this.btn3.UseVisualStyleBackColor = true;
+            this.btn3.Click += new System.EventHandler(this.btn3_Click);
+            // 
+            // btn2
+            // 
+            this.btn2.Font = new System.Drawing.Font("Microsoft Sans Serif", 48F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn2.Location = new System.Drawing.Point(208, 374);
+            this.btn2.Name = "btn2";
+            this.btn2.Size = new System.Drawing.Size(150, 150);
+            this.btn2.TabIndex = 11;
+            this.btn2.UseVisualStyleBackColor = true;
+            this.btn2.Click += new System.EventHandler(this.btn2_Click);
+            // 
+            // btn1
+            // 
+            this.btn1.Font = new System.Drawing.Font("Microsoft Sans Serif", 48F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn1.Location = new System.Drawing.Point(25, 374);
+            this.btn1.Name = "btn1";
+            this.btn1.Size = new System.Drawing.Size(150, 150);
+            this.btn1.TabIndex = 10;
+            this.btn1.UseVisualStyleBackColor = true;
+            this.btn1.Click += new System.EventHandler(this.btn1_Click);
+            // 
+            // labelVitoria
+            // 
+            this.labelVitoria.AutoSize = true;
+            this.labelVitoria.Location = new System.Drawing.Point(263, 556);
+            this.labelVitoria.Name = "labelVitoria";
+            this.labelVitoria.Size = new System.Drawing.Size(35, 13);
+            this.labelVitoria.TabIndex = 13;
+            this.labelVitoria.Text = "label1";
+            this.labelVitoria.Click += new System.EventHandler(this.labelVitoria_Click);
+            // 
+            // frmPrincipal
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(566, 578);
+            this.Controls.Add(this.labelVitoria);
+            this.Controls.Add(this.btn3);
+            this.Controls.Add(this.btn2);
+            this.Controls.Add(this.btn1);
+            this.Controls.Add(this.btn6);
+            this.Controls.Add(this.btn5);
+            this.Controls.Add(this.btn4);
+            this.Controls.Add(this.btn9);
+            this.Controls.Add(this.btn8);
+            this.Controls.Add(this.btn7);
+            this.Controls.Add(this.panel4);
+            this.Controls.Add(this.panel3);
+            this.Controls.Add(this.panel2);
+            this.Controls.Add(this.panel1);
+            this.Name = "frmPrincipal";
+            this.Text = "Jogo da Velha";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Panel panel2;
+        private System.Windows.Forms.Panel panel3;
+        private System.Windows.Forms.Panel panel4;
+        private System.Windows.Forms.Button btn7;
+        private System.Windows.Forms.Button btn8;
+        private System.Windows.Forms.Button btn9;
+        private System.Windows.Forms.Button btn6;
+        private System.Windows.Forms.Button btn5;
+        private System.Windows.Forms.Button btn4;
+        private System.Windows.Forms.Button btn3;
+        private System.Windows.Forms.Button btn2;
+        private System.Windows.Forms.Button btn1;
+        private System.Windows.Forms.Label labelVitoria;
+    }
+}
+
---
+++ JogodaVelha/frmPrincipal.cs
@@ -0,0 +1,299 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace JogodaVelha
+{
+    public partial class frmPrincipal : Form
+    {
+        public frmPrincipal()
+        {
+            InitializeComponent();
+        }
+
+        private Boolean turnoX = true;
+        private Boolean Jogo = true;
+
+        private string btnClicked;
+        
+
+        private bool checkTurno(bool x,string y)
+        {
+            if (turnoX == true && Jogo == true)
+            {
+                btn7.Text = "X";
+                return false;
+                labelVitoria.Text = "Turno = X";
+            }
+            else if (turnoX != true && Jogo == true)
+            {
+                btn7.Text = "O";
+                return true;
+                labelVitoria.Text = "Turno = Y";
+            }
+            return false;
+            
+        }
+
+
+        private void btn7_Click(object sender, EventArgs e)
+        {
+            btnClicked = "btn7";
+            
+           if (turnoX == true && Jogo == true)
+            {
+                btn7.Text = "X";
+                turnoX = false;
+            }
+            else if (turnoX != true && Jogo == true)
+            {
+                btn7.Text = "O";
+                turnoX = true;
+            }
+
+            if(btn7.Text == btn8.Text && btn7.Text == btn9.Text)
+            {
+                labelVitoria.Text = btn7.Text + "Venceu";
+                Jogo = false;
+            }
+            else if(btn7.Text == btn4.Text && btn7.Text == btn1.Text)
+            {
+                labelVitoria.Text = btn7.Text + "Venceu";
+                Jogo = false;
+            }
+            else if(btn7.Text == btn5.Text && btn7.Text == btn3.Text)
+            {
+                labelVitoria.Text = btn7.Text + "Venceu";
+                Jogo = false;
+            }
+        }
+
+        private void btn8_Click(object sender, EventArgs e)
+        {
+            if (turnoX == true && Jogo == true)
+            {
+                btn8.Text = "X";
+                turnoX = false;
+            }
+            else if (turnoX != true && Jogo == true)
+            {
+                btn8.Text = "O";
+                turnoX = true;
+            }
+
+            if (btn8.Text == btn7.Text && btn8.Text == btn9.Text)
+            {
+                labelVitoria.Text = btn8.Text + "Venceu";
+                Jogo = false;
+            }
+            else if (btn8.Text == btn5.Text && btn8.Text == btn2.Text)
+            {
+                labelVitoria.Text = btn8.Text + "Venceu";
+                Jogo = false;
+            }
+        }
+
+        private void btn9_Click(object sender, EventArgs e)
+        {
+            if (turnoX == true && Jogo == true)
+            {
+                btn9.Text = "X";
+                turnoX = false;
+            }
+            else if (turnoX != true && Jogo == true)
+            {
+                btn9.Text = "O";
+                turnoX = true;
+            }
+
+            if (btn9.Text == btn8.Text && btn9.Text == btn7.Text)
+            {
+                labelVitoria.Text = btn9.Text + "Venceu";
+                Jogo = false;
+            }
+            else if (btn9.Text == btn6.Text && btn9.Text == btn3.Text)
+            {
+                labelVitoria.Text = btn9.Text + "Venceu";
+                Jogo = false;
+            }
+            else if (btn9.Text == btn5.Text && btn9.Text == btn1.Text)
+            {
+                labelVitoria.Text = btn9.Text + "Venceu";
+                Jogo = false;
+            }
+        }
+
+        private void btn4_Click(object sender, EventArgs e)
+        {
+            if (turnoX == true && Jogo == true)
+            {
+                btn4.Text = "X";
+                turnoX = false;
+            }
+            else if (turnoX != true && Jogo == true)
+            {
+                btn4.Text = "O";
+                turnoX = true;
+            }
+
+            if (btn4.Text == btn5.Text && btn4.Text == btn6.Text)
+            {
+                labelVitoria.Text = btn4.Text + "Venceu";
+                Jogo = false;
+            }
+            else if (btn4.Text == btn7.Text && btn4.Text == btn1.Text)
+            {
+                labelVitoria.Text = btn4.Text + "Venceu";
+                Jogo = false;
+            }
+        }
+
+        private void btn5_Click(object sender, EventArgs e)
+        {
+            if (turnoX == true && Jogo == true)
+            {
+                btn5.Text = "X";
+                turnoX = false;
+            }
+            else if (turnoX != true && Jogo == true)
+            {
+                btn5.Text = "O";
+                turnoX = true;
+            }
+
+            if (btn5.Text == btn4.Text && btn5.Text == btn6.Text)
+            {
+                labelVitoria.Text = btn5.Text + "Venceu";
+                Jogo = false;
+            }
+            else if (btn5.Text == btn2.Text && btn5.Text == btn8.Text)
+            {
+                labelVitoria.Text = btn5.Text + "Venceu";
+                Jogo = false;
+            }
+           
+        }
+
+        private void btn6_Click(object sender, EventArgs e)
+        {
+            if (turnoX == true && Jogo == true)
+            {
+                btn6.Text = "X";
+                turnoX = false;
+            }
+            else if (turnoX != true && Jogo == true)
+            {
+                btn6.Text = "O";
+                turnoX = true;
+            }
+
+            if (btn6.Text == btn9.Text && btn6.Text == btn3.Text)
+            {
+                labelVitoria.Text = btn6.Text + "Venceu";
+                Jogo = false;
+            }
+            else if (btn6.Text == btn5.Text && btn6.Text == btn4.Text)
+            {
+                labelVitoria.Text = btn6.Text + "Venceu";
+                Jogo = false;
+            }
+        }
+
+        private void btn1_Click(object sender, EventArgs e)
+        {
+            if (turnoX == true && Jogo == true)
+            {
+                btn1.Text = "X";
+                turnoX = false;
+            }
+            else if (turnoX != true && Jogo == true)
+            {
+                btn1.Text = "O";
+                turnoX = true;
+            }
+
+            if (btn1.Text == btn4.Text && btn1.Text == btn7.Text)
+            {
+                labelVitoria.Text = btn1.Text + "Venceu";
+                Jogo = false;
+            }
+            else if (btn1.Text == btn5.Text && btn1.Text == btn9.Text)
+            {
+                labelVitoria.Text = btn1.Text + "Venceu";
+                Jogo = false;
+            }
+            else if (btn1.Text == btn2.Text && btn1.Text == btn3.Text)
+            {
+                labelVitoria.Text = this.Text + "Venceu";
+                Jogo = false;
+            }
+        }
+
+        private void btn2_Click(object sender, EventArgs e)
+        {
+            if (turnoX == true && Jogo == true)
+            {
+                btn2.Text = "X";
+                turnoX = false;
+            }
+            else if (turnoX != true && Jogo == true)
+            {
+                btn2.Text = "O";
+                turnoX = true;
+            }
+
+            if (btn2.Text == btn5.Text && btn2.Text == btn8.Text)
+            {
+                labelVitoria.Text = btn2.Text + "Venceu";
+                Jogo = false;
+            }
+            else if (btn2.Text == btn1.Text && btn2.Text == btn3.Text)
+            {
+                labelVitoria.Text = btn2.Text + "Venceu";
+                Jogo = false;
+            }
+            
+        }
+
+        private void btn3_Click(object sender, EventArgs e)
+        {
+            if (turnoX == true && Jogo == true)
+            {
+                btn3.Text = "X";
+                turnoX = false;
+            }
+            else if (turnoX != true && Jogo == true)
+            {
+                btn3.Text = "O";
+                turnoX = true;
+            }
+
+            if (btn3.Text == btn5.Text && btn3.Text == btn7.Text)
+            {
+                labelVitoria.Text = btn3.Text + "Venceu";
+                Jogo = false;
+            }
+            else if (btn3.Text == btn2.Text && btn3.Text == btn1.Text)
+            {
+                labelVitoria.Text = btn3.Text + "Venceu";
+                Jogo = false;
+            }
+            else if (btn3.Text == btn6.Text && btn3.Text == btn9.Text)
+            {
+                labelVitoria.Text = btn3.Text + "Venceu";
+                Jogo = false;
+            }
+        }
+
+        private void labelVitoria_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
